d9032a78d119569025e780f3828248bbac9ee5e4
serik <podelu000@gmail.com>
2025-02-16 16:45:47 +0300

    add postgress and diesel orm

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use axum::{response::Html, routing::get, Router};
+//use diesel::prelude::*;
+use diesel::pg::PgConnection;
+use diesel::r2d2::{self, ConnectionManager};
+use dotenvy::dotenv;
+use std::env;
 use std::fs;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(root));
+    dotenv().ok();
+
+    // Set up database connection pool
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool");
+
+    let app = Router::new().route("/", get(root)).with_state(pool);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
